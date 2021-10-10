b7901ab314a7a96a807adc1f4fabbab4109cd631
unknown <msg016@naver.com>
2021-10-11 00:58:28 +0900

    블럭 출력 형식 변경 및 회전 함수 추가

--- src/component/BlockUtil.js
+++ src/component/BlockUtil.js
@@ -4,10 +4,11 @@ const blockShape = [
     [[1,1],[1,1]],
     [[1,0,0],[1,1,1]],
     [[0,1,0],[1,1,1]],
-    [[0,1,0],[0,1,0]],
+    [[1],[1]],
     [[0,0,1],[1,1,1]],
-    [[0,1,0],[0,1,0],[0,1,0]],
+    [[1,1,1]],
     [[0,0,1],[0,0,1],[1,1,1]],
+    [[1,1,1],[0,0,1],[0,0,1]],
     [[1,1,1],[1,1,1],[1,1,1]],
 ]
 
@@ -28,21 +29,57 @@ const rotate = [
 
 const randomNumber = () => {
     return ({
-        shape: Math.floor(Math.random() * 10), 
+        shape: Math.floor(Math.random() * 11), 
         color: Math.floor(Math.random() * 5),
         rotate: Math.floor(Math.random() * 4)
     })
 }
 
+const rotateShape = (shape, rotate) => {
+    let rotated = []
+    const row = shape[0].length
+    const col = shape.length
+    switch (rotate) {
+        case 0:
+            return shape
+        case 90:
+            for (let i = 0; i < row; i++) {
+                let line = []
+                for (let j = col - 1; j >= 0; j--) {
+                    line.push(shape[j][i])
+                }
+                rotated.push(line)
+            }
+            break
+        case 180:
+            for (let i = col - 1; i >= 0; i--) {
+                let line = []
+                for (let j = row - 1; j >= 0; j--) {
+                    line.push(shape[i][j])
+                }
+                rotated.push(line)
+            }
+            break
+        case 270:
+            for (let i = row - 1; i >= 0; i--) {
+                let line = []
+                for (let j = 0; j < col; j++) {
+                    line.push(shape[j][i])
+                }
+                rotated.push(line)
+            }
+            break
+    }
+    return rotated
+}
+
 export const makeBlock = () => {
     const num = randomNumber()
-    const blockArray = blockShape[num.shape]
+    const blockArray = rotateShape(blockShape[num.shape], rotate[num.rotate])
     const color = blockColor[num.color]
-    const blockRotate = rotate[num.rotate]
 
     return {
         blockArray,
         color,
-        blockRotate
     }
 }
